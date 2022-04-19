b45ee519c20e379ab976e3085662c248fe30bf52
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-04-19 22:15:23 +0000

    add page introduction

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$log = "d-block";
+$display = "d-none";
+include 'include/header.php';
+
+?>
+<div class="container bg-light ps-5 d-flex flex-column align-items-start justify-content-center" style="height: 90%;">
+    <h1> Hello!</h1>
+    <div class="fs-4">
+        <p> <a href="inscription.php" class="text-decoration-none">Sign up </a> to start creating your contacts list.</p>
+    </div>
+    <div class="fs-4">
+        <p>Already have an account ? <a href="authenticate.php" class="text-decoration-none">Login here </a> .</p>
+    </div>
+
+
+
+
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
